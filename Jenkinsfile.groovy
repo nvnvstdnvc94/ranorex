06d64b62d62b2d31a581da59a7473ad4a644556a
nvnvstdnvc94 <97315725+nvnvstdnvc94@users.noreply.github.com>
2023-09-29 11:50:21 +0200

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout and Pull') {
+            steps {
+                script {
+                    // Navigirajte do direktorijuma sa vašim Git repozitorijumom
+                    dir('C:\\Users\\Nevena\\Documents\\Ranorex\\RanorexStudio Projects\\MyTest') {
+
+                        // Povucite najnovije promene sa udaljenog repozitorijuma
+                        bat 'git pull'
+                    }
+                }
+            }
+        }
+
+        stage('Run UFT Scenario') {
+            steps {
+                powershell '''
+                    cd "C:\\Users\\Nevena\\Documents\\Ranorex\\RanorexStudio Projects\\MyTest\\MyTest\\bin\\Debug"
+                    .\\MyTest.exe
+                   
+                '''
+                )
+            }
+        }
+
+ 
+
+    }
+}
